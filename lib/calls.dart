2de3d665aae85165fa5823095ff6c1efa3bef6cf
FlutLab.io <>
2022-05-31 06:30:29 +0000

    Autosave

--- lib/calls.dart
+++ lib/calls.dart
@@ -2,22 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class Chats extends StatelessWidget {
-  const Chats({Key? key}) : super(key: key);
-  Widget getChats(title, subTitle) {
-    return ListTile(
-      leading: CircleAvatar(
-        backgroundImage: NetworkImage("https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg"),
-        radius: 30,
-      ),
-      title: Text(
-        title,
-        style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500),
-      ),
-      subtitle: Text(subTitle),
-    );
-  }
-
+class Calls extends StatelessWidget {
+  const Calls({Key? key}) : super(key: key);
+  // Masukkan coding disini
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,39 +15,44 @@ class Chats extends StatelessWidget {
         },
         child: ListView(
           children: [
-            getChats("Ayang", "Selamat pagi :)"),
-            getChats("Ronaldo", "Today, 2:8 pm"),
-            getChats("Moh salah", "6 minutes ago"),
-            getChats("Rossi", "25 minutes ago"),
-            getChats("Torres", "Yestrday, 8:45 pm"),
-            getChats("Omar Hassan", "Yestrday, 5:5 Am"),
-            getChats("Sasuke", "Just now"),
-            getChats("Ronaldo", "Today, 2:8 pm"),
-            getChats("Moh salah", "6 minutes ago"),
-            getChats("Rossi", "25 minutes ago"),
-            getChats("Torres", "Yestrday, 8:45 pm"),
-            getChats("Omar Hassan", "Yestrday, 5:5 Am"),
-            getChats("Sasuke", "Just now"),
-            getChats("Ronaldo", "Today, 2:8 pm"),
-            getChats("Moh salah", "6 minutes ago"),
-            getChats("Rossi", "25 minutes ago"),
-            getChats("Torres", "Yestrday, 8:45 pm"),
-            getChats("Omar Hassan", "Yestrday, 5:5 Am"),
-            getChats("Sasuke", "Just now"),
-            getChats("Ronaldo", "Today, 2:8 pm"),
             SizedBox(
               height: 10,
             ),
+
+            // Masukkan Coding disini jangan rubah lainnya
+            //
+            //
+            //
+            //
+            //
+            //
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.green,
-        onPressed: () {},
-        child: Icon(
-          Icons.chat_bubble,
-          color: Colors.white,
-        ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.white,
+            focusElevation: 0.0,
+            onPressed: () {},
+            child: Icon(
+              Icons.video_call,
+              color: Colors.teal.shade900,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          FloatingActionButton(
+            backgroundColor: Colors.green,
+            onPressed: () {},
+            child: Icon(
+              Icons.add_call,
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
     );
   }
